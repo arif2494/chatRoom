8495d0a74d5833a5f51e56dbccc914f50957694b
Ariful Islam <arifulislam161024@gmail.com>
2021-11-23 12:54:53 +0600

    Shows messages and  activity on ui

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
 const chatForm = document.getElementById('chat-form');
 const messageInput = document.getElementById('msg');
 const socket = io();
-
+// message from server
 socket.on('message', (msg) => {
 	console.log(msg);
+	showMessage(msg);
 });
 
 // submit message
@@ -14,3 +15,14 @@ chatForm.addEventListener('submit', (e) => {
 	socket.emit('chatMessage', msg);
 	messageInput.value = '';
 });
+
+// show message to DOM
+const showMessage = (msg) => {
+	const div = document.createElement('div');
+	div.classList.add('message');
+	div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+     ${msg}
+    </p>`;
+	document.querySelector('.chat-messages').appendChild(div);
+};
